d4aa851926b4ed1f218586394793727a294629d1
GarySimons <garycsimons@gmail.com>
2020-08-15 09:14:27 +0000

    Added Model to Herstory app

--- herstories/models.py
+++ herstories/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Herstory(models.Model):
+    full_name = models.CharField(max_length=50, null=False, blank=False)
+    age = models.IntegerField(null=False, blank=False)
+    occupation = models.CharField(max_length=200, null=False, blank=False)
+    story = models.CharField(max_length=50, null=False, blank=False)
+
