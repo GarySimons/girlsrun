3a074b4f47d57cab4554a2e044915f6b5597a5b0
GarySimons <garycsimons@gmail.com>
2020-08-15 10:16:26 +0000

    Trying to add Herstory model to Admin, but getting errors

--- herstories/admin.py
+++ herstories/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Herstory
 
-# Register your models here.
+class HerstoryAdminInline(admin.TabularInline):
+    model = Herstory
+
+class HerstoryAdmin(admin.HerstoryAdmin):
+
+    fields = ('full_name', 'age', 'occupation', 'story',)
+
+    list_display = ('full_name', 'age', 'occupation', 'story',)
+
+admin.site.register(Herstory, HerstoryAdmin)
