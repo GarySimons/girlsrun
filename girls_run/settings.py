da2fb93faf6b425e4fdbfc584288cb840941cd00
GarySimons <garycsimons@gmail.com>
2020-08-26 04:28:11 +0000

    Set up Heroku girls-on-the-run app. Loaded events and herstory databases. Migrated apps

--- girls_run/settings.py
+++ girls_run/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 if os.path.exists('env.py'):
     import env
+import dj_database_url
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -130,10 +132,9 @@ WSGI_APPLICATION = 'girls_run.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
