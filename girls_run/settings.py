0ad0910231b3872738d6b136a4360f455a2d4bf6
GarySimons <garycsimons@gmail.com>
2020-08-28 06:20:44 +0000

    Testing to see if Heroku will automatically updated

--- girls_run/settings.py
+++ girls_run/settings.py
@@ -11,9 +11,10 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import dj_database_url
 if os.path.exists('env.py'):
     import env
-import dj_database_url
+
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
