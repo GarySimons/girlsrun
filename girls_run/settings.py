0130cd25c8abeecf8d8fd82640d707a77bb2e4f9
GarySimons <garycsimons@gmail.com>
2020-08-30 16:45:09 +0000

    added new heroku secret key and commented out import env in settings

--- girls_run/settings.py
+++ girls_run/settings.py
@@ -12,8 +12,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import dj_database_url
-if os.path.exists('env.py'):
-    import env
+#if os.path.exists('env.py'):
+#    import env
 
 
 
