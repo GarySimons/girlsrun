b193b40dcc822e91b569c7bf5a6107999d418889
GarySimons <garycsimons@gmail.com>
2020-08-31 06:27:02 +0000

    Fixed problems with Heroku and add_product.html not working

--- products/urls.py
+++ products/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.all_products, name='products'),
-    path('<int:product_id>/', views.product_detail, name='product_detail'),
-    path('add/', views.product_detail, name='add_product'),
+    path('<int:product_id>/', views.add_product, name='add_product'),
+    path('add/', views.add_product, name='add_product'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
-
 from .models import Product
 from .forms import ProductForm
 
