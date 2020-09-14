cb848eaeb145bb31addec58c3bbd626becac0098
GarySimons <garycsimons@gmail.com>
2020-09-14 08:34:35 +0000

    Added Active classes to nav links using '{% with url_name=request.resolver_match.url_name %}' and and if statement in link

--- girls_run/settings.py
+++ girls_run/settings.py
@@ -13,9 +13,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 import dj_database_url
 #if os.path.exists('env.py'):
-#    import env
-
-
+#    import en
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
