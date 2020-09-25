c92eecbbc02297d1b946ae1c1627e1c4614002b7
GarySimons <garycsimons@gmail.com>
2020-09-25 08:06:54 +0000

    Email sending, but not clearing when sent

--- static/js/sendEmail.js
+++ static/js/sendEmail.js
@@ -1,1 +1,17 @@
-console.log("Hello");
+function sendMail(contactForm) {
+    emailjs.send("girls on the run","Run", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "message": contactForm.message.value,
+
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;  
+}
