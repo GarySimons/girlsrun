ffe3e1339f1375ff27c3f839a3d6846310ceb08d
GarySimons <garycsimons@gmail.com>
2020-10-04 11:08:01 +0000

    Adjusted CSS margins on buttons and text to give correct spacing top and bottom

--- girls_run/settings.py
+++ girls_run/settings.py
@@ -101,7 +101,6 @@ TEMPLATES = [
 ]
 
 
-
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
 AUTHENTICATION_BACKENDS = [
@@ -226,4 +225,3 @@ else:
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
-    
