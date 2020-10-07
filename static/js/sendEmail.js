c71c6093b43aa835403cdd500975d07c4fa4b37c
GarySimons <garycsimons@gmail.com>
2020-10-07 14:07:20 +0000

    Added email sent message to contact form. Used jQuery hide and show function in my sendEmail.js file

--- static/js/sendEmail.js
+++ static/js/sendEmail.js
@@ -1,20 +1,33 @@
+// Hide 'email-sent' messaage
+$(document).ready(function () {
+                    $('#email-sent').hide();
+                });
+
+// Send email from form
 function sendMail(contactForm) {
-    emailjs.send("girls on the run","Run", {
+    emailjs.send("girls on the run", "Run", {
         "from_name": contactForm.name.value,
         "from_email": contactForm.emailaddress.value,
         "message": contactForm.message.value,
 
     })
-    .then(
-        function(response) {
-            console.log("SUCCESS", response);
-            contactForm.name.value = ""
-            contactForm.emailaddress.value = ""
-            contactForm.message.value = ""
-        },
-        function(error) {
-            console.log("FAILED", error);
-        }
-    );
-    return false;  
+        .then(
+            function (response) {
+                console.log("SUCCESS", response);
+                contactForm.name.value = ""
+                contactForm.emailaddress.value = ""
+                contactForm.message.value = ""
+
+                // show message when form sent
+                $(document).ready(function () {
+                    $('#email-sent').show();
+                });
+
+
+            },
+            function (error) {
+                console.log("FAILED", error);
+            }
+        );
+    return false;
 }
