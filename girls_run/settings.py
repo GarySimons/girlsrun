e43a85e10c5e3a953ce7442a92d2fb7f9e85cca7
GarySimons <garycsimons@gmail.com>
2020-10-11 17:24:47 +0000

    Changed DEBUG to False in settings.py. Change heading on Events

--- girls_run/settings.py
+++ girls_run/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = False
 
 ALLOWED_HOSTS = ['girls-on-the-run.herokuapp.com', 'localhost']
 
